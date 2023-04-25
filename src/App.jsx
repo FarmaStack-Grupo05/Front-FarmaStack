dfb4f3adbcf2246ae1742253089140771d2d2962
bbelen20001 <belenbrenda10@gmail.com>
2023-04-25 18:19:28 -0400

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Footer from "./Components/Footer/Footer";
-
+import Card from "./Components/Cards/Cards";
 function App() {
 
 
 	return (
 		<>
 			<h1>Estoy en App.js</h1>
+			<Card/>
 			<Footer/>
 		</>
 	);
