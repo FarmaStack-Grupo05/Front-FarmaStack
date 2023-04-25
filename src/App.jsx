fb8081bbcfa33b7956bd7eea00529e3e610e28cb
Cata Hernandez Mejia <110860662+Cata-hm@users.noreply.github.com>
2023-04-25 17:12:46 -0500

    navbar y Cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Footer from "./Components/Footer/Footer";
+import NavBar from "./Components/NavBar/NavBar";
+import Cards from "./Components/Cards/Cards";
 
 function App() {
-
-
-	return (
-		<>
-			<h1>Estoy en App.js</h1>
-			<Footer/>
-		</>
-	);
+  return (
+    <>
+      <NavBar />
+	  <Cards/>
+      <h1>Estoy en App.js</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
