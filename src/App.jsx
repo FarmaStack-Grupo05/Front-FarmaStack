a535e742e8278407903d67e492b7c73452b237ab
bbelen20001 <belenbrenda10@gmail.com>
2023-04-26 09:00:50 -0400

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,6 @@ import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Footer from "./Components/Footer/Footer";
-<<<<<<< HEAD
-import Card from "./Components/Cards/Cards";
-function App() {
-
-
-	return (
-		<>
-			<h1>Estoy en App.js</h1>
-			<Card/>
-			<Footer/>
-		</>
-	);
-=======
 import NavBar from "./Components/NavBar/NavBar";
 import Cards from "./Components/Cards/Cards";
 
@@ -27,7 +14,6 @@ function App() {
       <Footer />
     </>
   );
->>>>>>> refs/remotes/origin/main
 }
 
 export default App;
