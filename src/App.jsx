b878f7dfa913600b4c65fd708ce8c59f3cbd5a89
Alejo <jubany4@gmail.com>
2023-04-26 12:46:34 -0300

    changes footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
 import Cards from "./Components/Cards/Cards";
