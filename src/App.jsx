fef8b92347dea4d6dfa767b382da4fcfcdde1482
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-04-26 19:45:50 -0500

    for update branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
 import Cards from "./Components/Cards/Cards";
 import Pagination from "./Components/Pagination/Pagination";
+import CardsContainer from "./Components/CardsContainer/CardsContainer";
 
 function App() {
 	return (
 		<>
 			<NavBar />
 			<Cards />
-			<Footer />
+			<CardsContainer />
 			<Pagination />
+			<Footer />
 		</>
 	);
 }
