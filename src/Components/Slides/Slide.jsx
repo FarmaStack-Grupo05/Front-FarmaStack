5c6c4d0c11f03d7953854e6fd90e1ae4acf95d10
Alejo <jubany4@gmail.com>
2023-04-27 12:42:59 -0300

    Slides cargado y automatico

--- src/Components/Slides/Slide.jsx
+++ src/Components/Slides/Slide.jsx
@@ -1,65 +1,85 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../redux/slices/products/sliceProducts';
 
-const Slide =() => {
-
+const Slide = () => {
   const dispatch = useDispatch();
-  const {results} = useSelector((state) => state.productsState.list);
-const slides = [
-  {
-    url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-  },
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
 
-  {
-    url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-  },
-];
+  const { results } = useSelector(state => state.productsState.list);
+  let slides = [];
 
-const [currentIndex, setCurrentIndex] = useState(0);
+  if (Array.isArray(results)) {
+    slides = results.map(result => ({ url: result.image }));
+  }
 
-const prevSlide = () => {
-  const isFirstSlide = currentIndex === 0;
-  const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-  setCurrentIndex(newIndex);
-};
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
-const nextSlide = () => {
-  const isLastSlide = currentIndex === slides.length - 1;
-  const newIndex = isLastSlide ? 0 : currentIndex + 1;
-  setCurrentIndex(newIndex);
-};
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
 
-const goToSlide = (slideIndex) => {
-  setCurrentIndex(slideIndex);
-};
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
-return (
-  <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
-    <div
-      style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-      className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-    ></div>
-    {/* Left Arrow */}
-    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-      <BsChevronCompactLeft onClick={prevSlide} size={30} />
-    </div>
-    {/* Right Arrow */}
-    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+  const goToSlide = slideIndex => {
+    setCurrentIndex(slideIndex);
+  };
+
+  useEffect(() => {
+    const handleMouseEnter = () => {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    };
+    const handleMouseLeave = () => {
+      const newIntervalId = setInterval(() => {
+        nextSlide();
+      }, 5000);
+      setIntervalId(newIntervalId);
+    };
+    const newIntervalId = setInterval(() => {
+      nextSlide();
+    }, 5000);
+    setIntervalId(newIntervalId);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentIndex]);
+
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      {slides.length > 0 && (
+        <div
+          style={{
+            backgroundImage: `url(${slides[currentIndex].url})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
+          className="w-full h-full rounded-2xl bg-center bg-cover duration-50"
+        ></div>
+      )}
+
+      {/* Left Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
       <BsChevronCompactRight onClick={nextSlide} size={30} />
     </div>
     <div className='flex top-4 justify-center py-2'>
-      {results?.map((slide, slideIndex) => (
+      {slides?.map((slide, slideIndex) => (
         <div
           key={slideIndex}
           onClick={() => goToSlide(slideIndex)}
@@ -71,5 +91,6 @@ return (
     </div>
   </div>
 );
+
 }
 export default Slide
