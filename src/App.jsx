4b9531b0ad871ad77854bf2d5b0d04868d8898cf
Alejo <jubany4@gmail.com>
2023-04-27 08:54:18 -0300

    Create Slides

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
 import Cards from "./Components/Cards/Cards";
 import Pagination from "./Components/Pagination/Pagination";
+import Slide from "./Components/Slides/Slide";
 
 function App() {
 	return (
 		<>
 			<NavBar />
+			<Slide />
 			<Cards />
 			<Footer />
 			<Pagination />
