e8685246a1fb79c48ade529200294c687ab69d7c
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-04-27 18:40:38 -0500

    se soluciono el limite de darle next y previous

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -5,72 +5,79 @@ import { useDispatch, useSelector } from "react-redux";
 import { changePageProduct } from "../../redux/slices/products/sliceProducts";
 
 const Pagination = () => {
-  const dispatch = useDispatch();
-  const [page, setPage] = useState(1);
-  const handleChange = (e, p) => {
-    setPage(p);
-  };
-  useEffect(() => {
-    dispatch(changePageProduct(page));
-  }, [dispatch, page]);
+	const dispatch = useDispatch();
+	const [page, setPage] = useState(1);
 
-  const {results} = useSelector((state) => state.productsState.list);
-  
-  console.log(results)
-  return (
-    <div>
-      <div  className="inline-flex items-center justify-center gap-3">
-        <a
-          href="#"
-          className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 rtl:rotate-180"
-          onClick={(e) => handleChange(e, page - 1)}
-        >
-          <span className="sr-only">Next Page</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-3 w-3"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-    
+	useEffect(() => {
+		dispatch(changePageProduct(page));
+	}, [dispatch, page]);
 
-            <path
-              fillRule="evenodd"
-              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
+	const { results } = useSelector((state) => state.productsState.list);
+	const { next } = useSelector((state) => state.productsState.list);
+	const { previous } = useSelector((state) => state.productsState.list);
 
-          </svg>
-        </a>
+	const handleChangePrev = (e, p) => {
+		if (previous) {
+			setPage(p);
+		}
+	};
+	const handleChangeNext = (e, p) => {
+		if (next) {
+			setPage(p);
+		}
+	};
 
-        <p className="text-xs">
-          {page}<span className="mx-0.25">/</span> 
-         6
-        </p>
+	console.log(results);
+	return (
+		<div>
+			<div className="inline-flex items-center justify-center gap-3">
+				<a
+					href="#"
+					className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 rtl:rotate-180"
+					onClick={(e) => handleChangePrev(e, page - 1)}
+				>
+					<span className="sr-only">Next Page</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-3 w-3"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fillRule="evenodd"
+							d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</a>
 
-        <a
-          href="#"
-          className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 rtl:rotate-180"
-          onClick={(e) => handleChange(e,  page + 1)}
-        >
-          <span className="sr-only">Next Page</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-3 w-3"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </a>
-      </div>
-    </div>
-  );
-  
+				<p className="text-xs">
+					{page}
+					<span className="mx-0.25">/</span>6
+				</p>
+
+				<a
+					href="#"
+					className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 rtl:rotate-180"
+					onClick={(e) => handleChangeNext(e, page + 1)}
+				>
+					<span className="sr-only">Next Page</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-3 w-3"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fillRule="evenodd"
+							d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Pagination;
