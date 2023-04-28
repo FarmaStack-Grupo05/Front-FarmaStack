1b85d91508178d01247b5e9bb4c38827b29a82ed
Alejo <jubany4@gmail.com>
2023-04-27 22:00:45 -0300

    espacio home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,7 +4,6 @@ import NavBar from "../../Components/NavBar/NavBar";
 import Slides from '../../Components/Slides/Slide';
 import SlideBottom from "../../Components/Slides/SlideBottom";
 import Cards from "../Details/Details";
-
 import { Link } from "react-router-dom";
 
 
