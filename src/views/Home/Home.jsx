c9da481cf5de82db68ff04b953410fccb44c96c7
bbelen20001 <belenbrenda10@gmail.com>
2023-05-01 10:42:55 -0400

    add

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,11 +1,8 @@
 import Footer from "../../Components/Footer/Footer";
 import NavBar from "../../Components/NavBar/NavBar";
 import Slides from "../../Components/Slides/Slide";
-import SlideBottom from "../../Components/Slides/SlideBottom";
-import Cards from "../Details/Details";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import Products from "../Products/Products";
 import CardsHome from "../../Components/CardsHome/CardsHome";
 
 function Home() {
@@ -14,12 +11,7 @@ function Home() {
 		<>
 			<NavBar />
 			<Slides />
-<<<<<<< HEAD
-			<Cards />
-			<SlideBottom />
-=======
 			<CardsHome />
->>>>>>> developer
 			<Link to={"/farmastack/products"}>
 				<button>Products</button>
 			</Link>
