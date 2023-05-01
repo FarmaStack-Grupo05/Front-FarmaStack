2e6cf4e3e327b976672e3405b49fa13f85654598
bbelen20001 <belenbrenda10@gmail.com>
2023-05-01 11:36:45 -0400

    add

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,11 +2,10 @@ import Footer from "../../Components/Footer/Footer";
 import NavBar from "../../Components/NavBar/NavBar";
 import Slides from "../../Components/Slides/Slide";
 import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+
 import CardsHome from "../../Components/CardsHome/CardsHome";
 
 function Home() {
-	const location = useLocation();
 	return (
 		<>
 			<NavBar />
