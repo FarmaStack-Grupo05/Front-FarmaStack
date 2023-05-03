b5f068c1b1467f0729baa6bd6f96b233bc9d2ed5
Alejo <jubany4@gmail.com>
2023-05-03 12:30:33 -0300

    changes

--- src/views/FormProduct/FormProduct.jsx
+++ src/views/FormProduct/FormProduct.jsx
@@ -61,6 +61,7 @@ const FormProduct = () => {
 			}
 		}
 	};
+	console.log(inputs)
 	return (
 		<div>
 			{/*
@@ -166,13 +167,13 @@ const FormProduct = () => {
 								</div>
 								<div>
 									<label
-										class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-										for="file_input"
+										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+										htmlFor="file_input"
 									>
 										Upload file
 									</label>
 									<input
-										class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+										className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
 										id="file_input"
 										type="file"
 										onChange={handlerUpload}
