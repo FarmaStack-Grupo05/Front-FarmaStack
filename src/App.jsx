daa9425b809ebe3cd1e2214fdad95508d92a24df
bbelen20001 <belenbrenda10@gmail.com>
2023-05-03 00:32:35 -0400

    Contact

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
 } from "./views/index";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
+import ContactMe from "./Components/Contact/ContactMe";
 // npx tailwindcss -i ./src/style.css -o ./dist/output.css--watch  ***PARA ACTUALIZAR ESTILOS*********
 function App() {
 	return (
@@ -30,6 +31,7 @@ function App() {
 					<Route path="informacion-personal" element={<Profile />} />
 				</Route>
 				<Route exact path="/farmastack/products" element={<Products />} />
+				<Route exact path="/farmastack/contact" element={< ContactMe/>} />
 			</Routes>
 			<Footer />
 		</>
