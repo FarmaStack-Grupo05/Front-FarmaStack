805c158e052b370fc59f3f6eefd43022ead7886a
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-05-02 22:25:14 -0500

    cloudinary ok

--- src/views/index.js
+++ src/views/index.js
@@ -5,7 +5,15 @@ import Home from "./Home/Home";
 import Payment from "./Payment/Payment";
 import Profile from "./Profile/Profile";
 import Products from "./Products/Products";
+import FormProduct from "./FormProduct/FormProduct";
 
-
-
-export { AboutUs, Details, FormRegister, Home, Payment, Profile, Products, };
+export {
+	AboutUs,
+	Details,
+	FormRegister,
+	Home,
+	Payment,
+	Profile,
+	Products,
+	FormProduct,
+};
