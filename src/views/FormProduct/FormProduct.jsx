574903a45028c97073bcc42bdeed71d9cca1bee9
bbelen20001 <belenbrenda10@gmail.com>
2023-05-03 11:14:24 -0400

    Add

--- src/views/FormProduct/FormProduct.jsx
+++ src/views/FormProduct/FormProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import Swal from "sweetalert2";
 import axios from "axios";
 
@@ -166,13 +166,13 @@ const FormProduct = () => {
 								</div>
 								<div>
 									<label
-										class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-										for="file_input"
+										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+										htmlFor="file_input"
 									>
 										Upload file
 									</label>
 									<input
-										class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+										className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
 										id="file_input"
 										type="file"
 										onChange={handlerUpload}
