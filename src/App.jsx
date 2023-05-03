33b09471b2c9cbf49095733f77cf7ee79b147079
bbelen20001 <belenbrenda10@gmail.com>
2023-05-03 00:36:38 -0400

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,8 @@ function App() {
 					<Route path="informacion-personal" element={<Profile />} />
 				</Route>
 				<Route exact path="/farmastack/products" element={<Products />} />
-<<<<<<< HEAD
-				<Route exact path="/farmastack/contact" element={< ContactMe/>} />
-=======
 				<Route exact path="/farmastack/formProduct" element={<FormProduct />} />
->>>>>>> 805c158e052b370fc59f3f6eefd43022ead7886a
+				<Route exact path="/farmastack/contact" element={< ContactMe/>} />
 			</Routes>
 			<Footer />
 		</>
