484a84203f95bfc003ff32574d51310eff8153a6
Alejandra Betancourt <feellingokay@gmail.com>
2023-05-04 16:58:26 -0500

    paypal installation

--- src/views/Payment/Payment.jsx
+++ src/views/Payment/Payment.jsx
@@ -1,8 +1,31 @@
-function Payment() {
-	return (
-		<>
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-		</>
+const paypalOptions = {
+	"client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID, // Viene de Paypal
+	currency: "USD", // En que moneda se va a cobrar
+	intent: "capture", // Cobra de una vez
+};
+
+function Payment() {
+	const handleCreateOrder = (data, actions) => {
+		return actions.order.create({
+			purchase_units: [
+				{
+					amount: {
+						value: "100.00",
+					},
+				},
+			],
+		});
+	};
+
+	return (
+		<PayPalScriptProvider options={paypalOptions}>
+			<PayPalButtons
+				createOrder={handleCreateOrder}
+				style={{ layout: "horizontal" }}
+			/>
+		</PayPalScriptProvider>
 	);
 }
 
