be2c3ee9940f1f3ca35f7f461c40631668c2d2cd
jersitto <jersgonz310@outlook.com>
2023-05-04 09:02:21 -0500

    Add route for login

--- src/Components/NavBar/ProfileButton.jsx
+++ src/Components/NavBar/ProfileButton.jsx
@@ -38,7 +38,7 @@ function ProfileButton() {
         >
           <li>
             <a
-              href="#"
+              href="/farmastack/auth"
               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
               Log in
