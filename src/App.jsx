5c96ab095ea3a8542d164cbd261b35c3705b6444
bbelen20001 <belenbrenda10@gmail.com>
2023-05-05 09:31:05 -0400

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,19 @@ import {
 	Products,
 	FormProduct,
 } from "./views/index";
+import FilterProvider  from "./Components/Filter/FilterContext";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
 import LoginButton from "./Components/NavBar/loginButton";
 import ContactMe from "./Components/Contact/ContactMe";
 import LogoutButton from "./Components/NavBar/LogoutButton";
+
+
+
 // npx tailwindcss -i ./src/style.css -o ./dist/output.css--watch  ***PARA ACTUALIZAR ESTILOS*********
 function App() {
 	return (
+
 		<>
 			<NavBar />
 
@@ -33,7 +38,9 @@ function App() {
 				<Route path="/farmastack/profile" element={<Profile />}>
 					<Route path="informacion-personal" element={<Profile />} />
 				</Route>
+				<FilterProvider>
 				<Route exact path="/farmastack/products" element={<Products />} />
+				</FilterProvider>
 				<Route exact path="/farmastack/auth" element={<LoginButton/>}></Route>
 				<Route exact path="/farmastack/formProduct" element={<FormProduct />} />
 				<Route exact path="/farmastack/contact" element={< ContactMe/>} />
