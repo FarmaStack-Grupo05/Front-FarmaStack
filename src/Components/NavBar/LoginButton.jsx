e7532dbd46d3aa84f0be013c5ab9c078c29fc064
jersitto <jersgonz310@outlook.com>
2023-05-07 20:50:07 -0500

    User state info user

--- src/Components/NavBar/LoginButton.jsx
+++ src/Components/NavBar/LoginButton.jsx
@@ -1,21 +1,25 @@
-import  {useState} from "react"
+import  {useEffect, useState} from "react"
 import { useAuth0} from "@auth0/auth0-react"
 
 const LoginButton = ()=>{
     const [isOpen, setIsOpen] = useState(false)
 
-    const {loginWithRedirect, isAuthenticated} = useAuth0()
-
-
+    const {loginWithRedirect, isAuthenticated, getAccessTokenWithPopup} = useAuth0()
+    useEffect(()=>{ 
+        const setToken =async() =>{
+            const accessToken = await getAccessTokenWithPopup()
+            if(accessToken && isAuthenticated){
+                localStorage.setItem("authToken", accessToken)
+            }
+        }
+    }, [isAuthenticated])
 
     const handleTaggle = ()=> setIsOpen(!isOpen)
 
     const handleLogin = ()=> {
-        loginWithRedirect()
         console.log(isAuthenticated)
+        loginWithRedirect()
         setIsOpen(false)
- 
-      
     }
 
     return(
@@ -23,7 +27,7 @@ const LoginButton = ()=>{
             
           
             {!isOpen && (
-                <button onClick={handleLogin}>
+                <button onClick={handleLogin} >
                     Log In
                 </button>
             )}
