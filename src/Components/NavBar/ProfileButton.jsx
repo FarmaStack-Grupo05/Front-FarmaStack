a676a6d8d45a43278f712aca33946019cecc41c9
jersitto <jersgonz310@outlook.com>
2023-05-07 19:04:28 -0500

    profilebutton fixed

--- src/Components/NavBar/ProfileButton.jsx
+++ src/Components/NavBar/ProfileButton.jsx
@@ -41,7 +41,7 @@ function ProfileButton() {
 				>
 					<li>
 						<a
-							href="/farmastack/auth"
+							href=""
 							className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
 						>
 							<LoginButton/>
