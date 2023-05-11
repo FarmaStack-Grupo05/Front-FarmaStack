cef4d9b2fae73f8c271cc845d83ecd2eeb249f4e
bbelen20001 <belenbrenda10@gmail.com>
2023-05-11 18:15:24 -0400

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import {
-	AboutUs,
-	Dashboard,
-	Details,
-	Home,
-	Payment,
-	Products,
-	Profile,
+  AboutUs,
+  Dashboard,
+  Details,
+  Home,
+  Payment,
+  Products,
+  Profile,
 } from "./views/index";
 
 import { useAuth0 } from "@auth0/auth0-react";
@@ -14,63 +14,70 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import ContactMe from "./Components/Contact/ContactMe";
 import Footer from "./Components/Footer/Footer";
-import LoginButton from "./Components/NavBar/LoginButton";
+
 import NavBar from "./Components/NavBar/NavBar";
 import { getCart } from "./redux/slices/cart/sliceCart";
 import { getUser } from "./redux/slices/users/sliceUsers";
 import FormRegister from "./Components/FormRegister/FormRegister";
-
+import { useNavigate } from "react-router-dom";
 // npx tailwindcss -i ./src/style.css -o ./dist/output.css--watch  ***PARA ACTUALIZAR ESTILOS*********
 function App() {
-	const location = useLocation();
-	const dispatch = useDispatch();
-	const { getAccessTokenSilently, isAuthenticated, user } = useAuth0();
+  
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { getAccessTokenSilently, isAuthenticated, user } = useAuth0();
+  const navigate = useNavigate();
+  const refreshPage = () => {
+    navigate(0);
+}
+  const sarasa = localStorage.getItem("token");
+  console.log(sarasa);
 
-	useEffect(() => {
-		const getAccessToken = async () => {
-			const accessToken = await getAccessTokenSilently();
-			localStorage.setItem("token", accessToken);
-		};
+  useEffect(() => {
+    const getAccessToken = async () => {
+      const accessToken = await getAccessTokenSilently();
+      localStorage.setItem("token", accessToken);
+    };
 
-		if (isAuthenticated) {
-			getAccessToken();
-			dispatch(getUser(user));
-			dispatch(getCart(user.sub));
-		} else {
-			localStorage.removeItem("token");
-			dispatch(getUser({}));
-		}
-	}, [isAuthenticated]);
+    if (isAuthenticated) {
+      getAccessToken();
+      dispatch(getUser(user));
+      dispatch(getCart(user.sub));
+    } else {
+      localStorage.removeItem("token");
+      dispatch(getUser({}));
+    }
+  }, [isAuthenticated]);
 
-	return (
-		<>
-			{location.pathname === "/dashboard" ? (
-				<Routes>
-					<Route exact path="/dashboard" element={<Dashboard />} />
-				</Routes>
-			) : (
-				<>
-					<NavBar />
-					<Routes>
-						<Route exact path="/" element={<Home />} />
-						<Route exact path="/farmastack/aboutus" element={<AboutUs />} />
-						<Route exact path="/farmastack/details/:id" element={<Details />} />
-						<Route exact path="/farmastack/payment" element={<Payment />} />
-						<Route path="/farmastack/profile" element={<Profile />} />
-						<Route exact path="/farmastack/products" element={<Products />} />
-						<Route exact path="/farmastack/contact" element={<ContactMe />} />
-						<Route exact path="/farmastack/auth" element={<LoginButton />} />
-						<Route
-							exact
-							path="farmastack/formRegister"
-							element={<FormRegister />}
-						/>
-					</Routes>
-					<Footer />
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      {location.pathname === "/dashboard" ? (
+        <Routes>
+          {sarasa && <Route exact path="/dashboard" element={<Dashboard />} />}
+        </Routes>
+      ) : (
+        <>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/farmastack/aboutus" element={<AboutUs />} />
+            <Route exact path="/farmastack/details/:id" element={<Details />} />
+            <Route exact path="/farmastack/payment" element={<Payment />} />
+            {sarasa && <Route  path="/farmastack/profile" element={<Profile />} />}
+            <Route exact path="/farmastack/products" element={<Products />} />
+            <Route exact path="/farmastack/contact" element={<ContactMe />} />
+
+            {sarasa && <Route
+              exact
+              path="farmastack/formRegister"
+              element={<FormRegister />}
+            />}
+          </Routes>
+          <Footer />
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/Components/NavBar/LoginButton.jsx
+++ src/Components/NavBar/LoginButton.jsx
@@ -1,36 +1,23 @@
-import { useEffect, useState } from "react"
-import { useAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const LoginButton = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const { loginWithPopup, isAuthenticated, getAccessTokenWithPopup } = useAuth0()
-
-    useEffect(() => {
-        const setToken = async () => {
-            const accessToken = await getAccessTokenWithPopup()
-            if (accessToken && isAuthenticated) {
-                localStorage.setItem("authToken", accessToken)
-            }
-        }
-    }, [isAuthenticated])
-
-    const login = (e) => {
-        e.preventDefault();
-        loginWithPopup();
-      };
-
-    return (
-        <div>
+ 
+  const { loginWithPopup } = useAuth0();
 
 
-            {!isOpen && (
-                <button onClick={login} >
-                    Log In
-                </button>
-            )}
+  const login = (e) => {
+   
+    e.preventDefault();
+    loginWithPopup();
+  };
 
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={login}>Log In</button>
+     
+    </div>
+  );
+};
 
-export default LoginButton
+export default LoginButton;
