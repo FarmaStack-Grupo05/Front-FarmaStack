0ab324b72817c2b891342b99a5114a620e016fb8
malbet <feellingokay@gmail.com>
2023-05-12 02:17:41 -0400

    Adds payments success page styles

--- src/views/PaymentSuccess/PaymentSuccess.jsx
+++ src/views/PaymentSuccess/PaymentSuccess.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
 function PaymentSuccess() {
   const { paymentId } = useParams();
   const [payment, setPayment] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const cart = useSelector((state) => state.cartState);
   const { dataBaseUser: user } = useSelector((state) => state.userState);
 
@@ -17,6 +18,7 @@ function PaymentSuccess() {
         },
       });
       setPayment(data);
+      setIsLoading(false);
     }
 
     if (user) {
@@ -24,56 +26,84 @@ function PaymentSuccess() {
     }
   }, [cart.products, paymentId, user])
 
-  return (
-    <div className="py-6 px-4 flex flex-col">
-      <h1 className="text-2xl font-bold">Payment Success</h1>
-      <div className="flex flex-col mt-4">
-        <div className="flex flex-col">
-          <h2 className="text-xl font-bold">Order Details</h2>
-          <div className="flex flex-col mt-4">
-            <div className="flex flex-col">
-              <h3 className="text-lg font-bold">Products</h3>
-              <div className="flex flex-col mt-4">
-                {payment?.OrderItems.map((product) => (
-                  <div className="flex flex-col" key={product.id}>
-                    <div className="flex flex-col">
-                      <h4 className="text-md font-bold">{product.name}</h4>
-                      <div className="flex flex-col mt-4">
-                        <div className="flex flex-col">
-                          <p className="text-md font-bold">Quantity: {product.quantity}</p>
-                          <p className="text-md font-bold">Price: ${product.price}</p>
-                          <p className="text-md font-bold">Subtotal: ${product.subtotal}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-                <div className="flex flex-col">
-                  <h4 className="text-md font-bold">Total: ${payment?.total_price}</h4>
-                  <h4 className="text-md font-bold">Payment ID: {payment?.payment_id}</h4>
-                  {/* <h4 className="text-md font-bold">Status: {payment?.status}</h4> */}
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col mt-4">
-              <h3 className="text-lg font-bold">User</h3>
-              <div className="flex flex-col mt-4">
-                <div className="flex flex-col">
-                  <p className="text-md font-bold">Name: {user.name}</p>
-                  <p className="text-md font-bold">Address: {user.address}</p>
-                  {/* <p className="text-md font-bold">Name: {payment?.user.name}</p>
-                  <p className="text-md font-bold">Lastname: {payment?.user.lastname}</p>
-                  <p className="text-md font-bold">Email: {payment?.user.email}</p>
-                  <p className="text-md font-bold">Address: {payment?.user.address}</p>
-                  <p className="text-md font-bold">City: {payment?.user.city}</p>
-                  <p className="text-md font-bold">State: {payment?.user.state}</p>
-                  <p className="text-md font-bold">Country: {payment?.user.country}</p> */}
-                </div>
-              </div>
-            </div>
-          </div>
+  if (isLoading) {
+    return (
+      <div className="py-6 px-4 flex flex-col items-center">
+        <div className="max-w-2xl shadow-lg w-full px-4 md:px-6 py-4 border">
+          <h1 className="text-2xl font-bold">Loading...</h1>
         </div>
       </div>
+    )
+  }
+
+  return (
+    <div className="py-6 px-4 flex flex-col items-center">
+      <div className="max-w-2xl shadow-lg w-full px-4 md:px-6 py-4 border">
+        <p className="text-sm text-gray-700 my-2">
+          Payment ID: {payment?.payment_id}
+        </p>
+        <h1 className="text-2xl font-bold">Payment Success</h1>
+        <p className="text-md text-gray-700 my-2">
+          Congratulations! Your payment was successful.
+          Here are the details of your purchase:
+        </p>
+
+        <h2 className="text-xl font-bold mt-4">Products:</h2>
+        <ul>
+          {payment?.OrderItems.map(({ Product, subtotal, price, quantity }) => (
+            <li
+              className="flex justify-between items-center py-4 px-8 border-b"
+              key={Product.id}
+            >
+              <div className="flex items-center">
+                <img
+                  src={Product.image}
+                  alt={Product.name}
+                  className="w-16 h-16 object-cover rounded-full"
+                />
+                <div className="ml-4">
+                  <h2 className="text-md font-bold">{Product.name}</h2>
+                  <p className="text-sm text-gray-700">
+                    Quantity:{" "}
+                    <span className="font-bold">{quantity}</span>
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col items-end">
+                <p className="font-bold">${subtotal}</p>
+                <p className="text-sm text-gray-500">
+                  ${price}/each one
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-between items-center py-4 px-8">
+          <h1 className="text-xl font-semibold">Total:</h1>
+          <p className="text-2xl font-bold">${payment.total_price}</p>
+        </div>
+      </div>
+
+      <h2 className="text-xl font-bold mt-4">Delivery:</h2>
+      <div className="max-w-2xl shadow-lg w-full px-4 md:px-6 py-4 border">
+        <p className="text-md text-gray-500 my-2">
+          Your order will be delivered in the following 2-4 days to:
+        </p>
+        <p className="text-md text-gray-700 my-2">
+          <span className="font-bold">Name: </span>
+          {user?.name}
+        </p>
+        <p className="text-md text-gray-700 my-2">
+          <span className="font-bold">Address: </span>
+          {user?.address}
+        </p>
+      </div>
+      <Link
+        to="/farmastack/products"
+        className="font-medium text-green-600 hover:text-green-500 mt-6"
+      >
+        Continue Shopping
+      </Link>
     </div>
   )
 }
